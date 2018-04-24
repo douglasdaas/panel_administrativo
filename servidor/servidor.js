9c878427b09177867ce4b4ad28e05ede23b27272
Douglas Acosta <douglasdaas@gmail.com>
2018-04-24 19:49:56 -0400

    Se agrego el POST de empresas

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -14,18 +14,15 @@ var app = express();// Se inicia express en la variable app
 
 app.use(bodyParser.json());
 
-app.post('/reportes', (req, res) =>{ //<--- la ruta /reporte es para crear un nuevo reporte
-  var reporte = new Reporte({
+app.post('/empresas', (req, res) =>{ //<--- la ruta /reporte es para crear una nueva empresa
+  var reporte = new Empresa({
     Nombre: req.body.Nombre,
-    Descripcion: req.body.Descripcion,
-    Monto: req.body.Monto,
-    Moneda: req.body.Moneda,
-    Tipo: req.body.Tipo
+    RIF: req.body.RIF
   });
 
-  reporte.save().then( (reporte) => { //<----- guarda un nuevp reporte
-    res.send(reporte);
-  }, (erre) => {
+  reporte.save().then( (empresa) => { //<----- guarda una nueva empresa
+    res.send(empresa);
+  }, (error) => {
     res.status(400).send(error);// envia el error del guardado
   })
 
